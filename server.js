ef6382aead3e0b9fcc2d2d3ece0808cc746aaaa3
jjanahassan <janahassan210@yahoo.com>
2026-07-24 12:40:20 +0300

    Stage 4: full CRUD

--- server.js
+++ server.js
@@ -63,6 +63,53 @@ app.post("/tasks", (req,res) => {
 
 });
 
+app.put("/tasks/:id", (req,res) => {
+    const id= Number(req.params.id);
+    const task= tasks.find(task => task.id===id);
+
+    if (!task) {
+        return res.status(404).json({
+            error: `Task ${id} not found`
+        });
+    }
+
+    const {title, done}= req.body;
+    if (!req.body || Object.keys(req.body).length===0){
+        return res.status(400).json({
+            error: "Request body cannot be empty"
+        });
+    }
+
+    if (title.trim() === "") {
+        return res.status(400).json({
+                error: "Title cannot be empty"
+            });
+    }
+
+    task.title= title.trim();
+
+    if (done!== undefined){
+        task.done=done;
+    }
+
+    res.json(task);
+});
+
+app.delete("/tasks/:id", (req,res) => {
+    const id= Number(req.params.id);
+    const taskIndex= tasks.findIndex(task => task.id ===id);
+ 
+     if (taskIndex === -1) {
+        return res.status(404).json({
+            error: `Task ${id} not found`
+        });
+    }
+
+    tasks.splice(taskIndex, 1);
+    res.status(204).send();
+
+});
+
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
