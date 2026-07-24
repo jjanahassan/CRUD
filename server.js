922d9d28b783651e6b30e7838c07610e6b96b285
jjanahassan <janahassan210@yahoo.com>
2026-07-24 12:18:29 +0300

    Stage 3: create with validation

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const port = 3000;
 
@@ -44,6 +45,22 @@ app.get('/tasks/:id', (req, res) => {
     }
     
     res.json(task);
+});
+
+app.post("/tasks", (req,res) => {
+    const {title}=req.body;
+
+    if(!title || title.trim() === ""){
+        return res.status(400).json({
+            error: "Title is required"
+        });
+    }
+
+    const newTask={id: tasks.length+1, title, done: false};
+    tasks.push(newTask);
+
+    res.status(201).json(newTask);
+
 });
 
 app.listen(port, () => {
